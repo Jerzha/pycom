5e114d55afbebc766cabe56a84b34bbafbf439ab
Jeffry Zhang <zhanghao@rock-chips.com>
2017-03-28 21:41:28 +0800

    init repo

---
+++ com.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from time import sleep
+
+import serial
+import signal
+import sys
+import thread
+
+dev = sys.argv[1]
+se = serial.Serial(dev, 1500000)
+is_exit = False
+buff = ""
+
+
+def handle_sigint_tstp(sig, f):
+    se.write(chr(0x03))
+
+
+def exit_com():
+    global is_exit
+    is_exit = True
+    se.close()
+
+signal.signal(signal.SIGINT, handle_sigint_tstp)
+signal.signal(signal.SIGTSTP, handle_sigint_tstp)
+
+
+def input_thread():
+    global is_exit, se
+    while not is_exit:
+        cmd = sys.stdin.readline()
+        if cmd.strip() == 'quit':
+            exit_com()
+        se.write(cmd)
+
+
+def output_thread():
+    global is_exit, se
+    while not is_exit:
+        line = se.readline()
+        print str(line).rstrip('\n')
+
+if not se.is_open:
+    print 'Cannot open', dev
+    exit(-1)
+
+th1 = thread.start_new_thread(input_thread, ())
+th2 = thread.start_new_thread(output_thread, ())
+
+th1.join()
+th2.join()
+print "xxxxx"
